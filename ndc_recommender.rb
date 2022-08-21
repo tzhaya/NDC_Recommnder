ad0db68960d6eefc99529fd353233696d27dc8e4
Takanori Hayashi <14845980+tzhaya@users.noreply.github.com>
2022-08-21 17:15:16 +0900

    #2 ファイルからの入力に対応

--- ndc_recommender.rb
+++ ndc_recommender.rb
@@ -11,11 +11,11 @@ class AppOption
   def initialize
     @options = {}
     OptionParser.new do |o|
-    #  o.on('-c', '--csv', 'output csv') { |v| @options[:csv] = v }
-    #  o.on('-v', '--verpose', 'output verpose') { |v| @options[:verpose] = v }
-      o.on('-t', '--title', 'add title to predict') { |v| @options[:title] = v }
-      o.on('-u', '--uri item', 'set URI') { |v| @options[:uri] = v }
-      o.on('-h', '--help', 'show this help') {|v| puts o; exit }
+      o.on('-i VALUE',   'input file name') { |v| @options[:inputfilename] = v }
+      o.on('-t',         'add title to predict') { |v| @options[:title] = v }
+      o.on('-o [VALUE]', 'output CSV file name') { |v| @options[:outfilename] = v ; }
+      o.on('-u VALUE',   'set URI') { |v| @options[:uri] = v }
+      o.on('-h',         'show this help') {|v| puts o; exit }
       o.parse!(ARGV)
     end
   end
@@ -34,12 +34,58 @@ class AppOption
   def get_extras
     ARGV
   end
+  
 end
 
 option = AppOption.new
 
 if option.has?(:uri)
   uri = option.get(:uri)
+  if URI.regexp.match(uri).nil?
+    puts "You must set URI. usage: -u [URI]."
+    exit
+  end
+else
+  if option.has?(:inputfilename) 
+  then
+
+  else
+    puts "You must set URI. usage: -u [URI]."
+    exit
+end
+
+# CSVで出力
+
+if option.has?(:outfilename)
+  outfilename   = option.get(:outfilename)
+  inputfilename = option.get(:inputfilename)
+    if 
+      outfilename.nil? 
+      then 
+        outfilename = "outfile.csv"
+    end
+    CSV.open(outfilename,'w') do |csv|
+      csv << ["URL","Title","Description","NDC 1","NDC 1 Label","NDC 1 score","NDC 2","NDC 2 label","NDC 2 score","NDC 3","NDC 3 label","NDC 3 score"]        ##ヘッダ
+    
+    File.foreach(inputfilename){|uri|
+      page = MetaInspector.new(uri)
+      if page.description != "" then
+        param = page.description
+          else
+            param = page.title    
+      end
+      res = Net::HTTP.post_form(URI.parse('https://lab.ndl.go.jp/ndc/api/predict'),{ 'bib' => param })
+      result = JSON.parse(res.body)
+
+      csv << [uri, page.title, page.description,
+              "#{result[0]["value"]}","#{result[0]["label"]}","#{result[0]["score"]}",
+              "#{result[1]["value"]}","#{result[1]["label"]}","#{result[1]["score"]}",
+              "#{result[2]["value"]}","#{result[2]["label"]}","#{result[2]["score"]}"]
+      }
+      sleep 1
+      end
+
+    exit
 end
 
 # 指定されたWebページの内容を取得
@@ -50,14 +96,15 @@ puts "Description:\r\n" + page.description
 
 if page.description != "" then
 # -t でタイトルを加える指定があればtitleとdescription、なければdescritionのみをNDCpredicterのAPIにPOSTで送信
-  if option.has?(:title)
+  if option.has?(:title)  then
     param = page.title + page.description
-    #puts "send title and description to NDL"
-  else
+    else
     param = page.description
-    #puts "send description to NDL"
   end
   res = Net::HTTP.post_form(URI.parse('https://lab.ndl.go.jp/ndc/api/predict'),{ 'bib' => param })
+  else
+  puts "No description."
+end
 
 # 結果がJSONで返ってくるのでparse
   result = JSON.parse(res.body)
@@ -66,6 +113,4 @@ if page.description != "" then
   puts "#{result[0]["value"]} / #{result[0]["label"]}"
   puts "#{result[1]["value"]} / #{result[1]["label"]}"
   puts "#{result[2]["value"]} / #{result[2]["label"]}"
- else
-  puts "No description"
 end
